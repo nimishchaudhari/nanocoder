6b6fb5836338e7dd60e581d35e5d06f7c40fcf17
Will Lamerton <william.lamerton@gmail.com>
2025-08-23 17:36:03 +0100

    mod: continuing to style sending messages to the chat etc

--- source/components/user-message.tsx
+++ source/components/user-message.tsx
@@ -6,16 +6,35 @@ interface UserMessageProps {
 }
 
 export default function UserMessage({message}: UserMessageProps) {
+	// Show placeholder for long messages
+	if (message.length > 150) {
+		const lineCount = message.split('\n').length;
+		return (
+			<Box flexDirection="column" marginBottom={1}>
+				<Box>
+					<Text color={colors.primary} bold>
+						You:
+					</Text>
+				</Box>
+				<Box>
+					<Text color={colors.secondary}>
+						[{message.length} characters, {lineCount} lines]
+					</Text>
+				</Box>
+			</Box>
+		);
+	}
+
 	const lines = message.split('\n');
-	
+
 	return (
 		<Box flexDirection="column" marginBottom={1}>
 			<Box>
-				<Text color={colors.secondary} bold>
+				<Text color={colors.primary} bold>
 					You:
 				</Text>
 			</Box>
-			<Box flexDirection="column" paddingLeft={1}>
+			<Box flexDirection="column">
 				{lines.map((line, index) => (
 					<Text key={index} color={colors.white}>
 						{line}
